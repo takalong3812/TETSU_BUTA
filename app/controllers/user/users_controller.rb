e97497fbc7c95c10c46bec580da1411a934b09bf
EC2 Default User <ec2-user@ip-172-31-6-235.ap-northeast-1.compute.internal>
2023-01-21 08:08:14 +0000

    sign_up,in OK

---
+++ app/controllers/user/users_controller.rb
@@ -0,0 +1,9 @@
+class User::UsersController < ApplicationController
+    
+ def show
+  @user= User.find(params[:id])
+   
+ end
+ 
+ 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "homes#top"
+
+  devise_for :users, controllers:{
+   registrations: "user/registrations",
+   sessions: 'user/sessions'
+  }
+  
+  devise_for :admin,skip: [:registration, :passwords] ,controllers:{
+    sessions: "admin/sessions"
+  }
+  
+  namespace :user do
+  end
+  
+  
+  namespace :admin do
+  end
+  
+  
+  
 end
