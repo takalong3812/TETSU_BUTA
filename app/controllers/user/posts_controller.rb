9ac4be82028483761f8264e1edcee66df04a3413
EC2 Default User <ec2-user@ip-172-31-6-235.ap-northeast-1.compute.internal>
2023-01-23 13:49:50 +0000

    詳細画面実装

--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -3,6 +3,7 @@ class User::PostsController < ApplicationController
     def create
      @post= Post.new(post_params)
      @post.user_id= current_user.id
+  
       if @post.save
       flash[:notice]= '投稿に成功しました'
       redirect_to user_post_path(@post.id)
@@ -15,19 +16,17 @@ class User::PostsController < ApplicationController
     
     def index
      @posts= Post.all
-     @post= Post.new
-     @user= current_user
     end
     
     def show
-     @post= Post.find(:params[:id])
+     @post= Post.find(params[:id])
      @user= @post.user
      @new_post= Post.new
     end 
     
   private
     def post_params
-     prams.require(:post).permit(:title, :impression, :image, :user_id)
+     params.require(:post).permit(:title, :impression, :image, :user_id)
     end   
     
      
