f84fcf28bb7d367d52b5771f0b396f84773de25d
EC2 Default User <ec2-user@ip-172-31-6-235.ap-northeast-1.compute.internal>
2023-02-01 14:17:33 +0000

    管理者ページ作成中

--- app/controllers/user/guest_controller.rb
+++ app/controllers/user/guest_controller.rb
@@ -1,7 +1,6 @@
 class User::GuestController < ApplicationController
  def new_guest
      user= User.find_or_create_by!(email: 'guest@example.com') do |user|
-        user.image= image_tag'no_image.jpg', size: "100x100" 
         user.password= SecureRandom.urlsafe_base64 
         user.password_confirmation= user.password
         user.name= 'guest'
